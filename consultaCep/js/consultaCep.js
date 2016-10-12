427880d5c2642759607ee14d1f58490cc3fe6780
cjuniorr <celiopaivajr@gmail.com>
2016-10-12 19:56:58 -0300

    Alterações
    Estilo da página adicionado e funcionamento OK.

--- consultaCep/js/consultaCep.js
+++ consultaCep/js/consultaCep.js
@@ -1,5 +1,38 @@
+$(document).ready(function() {
 
-var cep = document.getElementById('form');
+            $("#divEndereco").hide();
+            
+            $("#btnConsultar").click(function() {
 
+                    if (verificaInputVazio($("#texCep").val()) == false){
+                      return;
+                    }
+                        var cep = $("#texCep").val();
+                        var url = "http://apps.widenet.com.br/busca-cep/api/cep/" + cep + ".json";
 
-$.getScript("http://cep.republicavirtual.com.br/web_cep.php?cep="+ cep + "&formato=json");
+                        $.get(url, {
+                                code: cep
+                            },
+                            function(result) {
+                                if (result.status != 1) {
+                                    alert(result.message || "Houve um erro desconhecido!");
+                                    $("#divEndereco").hide();
+                                    return;
+                                }
+                                $("#txtRua").text(result.address);
+                                $("#txtBairro").text(result.district);
+                                $("#txtCidade").text(result.city);
+                                $("#txtEstado").text(result.state);
+                                $("#txtCEP").text(result.code);
+
+                                $("#divEndereco").show();
+                            });
+                    });
+
+            function verificaInputVazio(a) {
+                if (a == "" || null) {
+                    alert("CEP inválido!");
+                    return false;
+                }
+            }
+            });
