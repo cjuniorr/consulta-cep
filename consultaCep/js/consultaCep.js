f62931c9fbc19d079ed7cbd550fd3c71bf0dcaa1
cjuniorr <celiopaivajr@gmail.com>
2016-10-12 22:04:44 -0300

    Começo de desenvolvimento
    Início da implementação do google maps

--- consultaCep/js/consultaCep.js
+++ consultaCep/js/consultaCep.js
@@ -1,38 +1,89 @@
 $(document).ready(function() {
+    var map;
+    var maker;
 
-            $("#divEndereco").hide();
-            
-            $("#btnConsultar").click(function() {
+    $("#map-canvas").show();
+    $("#divEndereco").hide();
 
-                    if (verificaInputVazio($("#texCep").val()) == false){
-                      return;
-                    }
-                        var cep = $("#texCep").val();
-                        var url = "http://apps.widenet.com.br/busca-cep/api/cep/" + cep + ".json";
+    $("#btnConsultar").click(function() {
 
-                        $.get(url, {
-                                code: cep
-                            },
-                            function(result) {
-                                if (result.status != 1) {
-                                    alert(result.message || "Houve um erro desconhecido!");
-                                    $("#divEndereco").hide();
-                                    return;
-                                }
-                                $("#txtRua").text(result.address);
-                                $("#txtBairro").text(result.district);
-                                $("#txtCidade").text(result.city);
-                                $("#txtEstado").text(result.state);
-                                $("#txtCEP").text(result.code);
+        if (verificaInputVazio($("#texCep").val()) == false) {
+            return;
+        }
+        var cep = $("#texCep").val();
+        var url = "http://apps.widenet.com.br/busca-cep/api/cep/" + cep + ".json";
 
-                                $("#divEndereco").show();
-                            });
-                    });
-
-            function verificaInputVazio(a) {
-                if (a == "" || null) {
-                    alert("CEP inválido!");
-                    return false;
+        $.get(url, {
+                code: cep
+            },
+            function(result) {
+                if (result.status != 1) {
+                    alert(result.message || "Houve um erro desconhecido!");
+                    $("#divEndereco").hide();
+                    return;
                 }
-            }
+                $("#txtRua").text(result.address);
+                $("#txtBairro").text(result.district);
+                $("#txtCidade").text(result.city);
+                $("#txtEstado").text(result.state);
+                $("#txtCEP").text(result.code);
+
+                $("#divEndereco").show();
             });
+    });
+
+    function verificaInputVazio(a) {
+        if (a == "" || null) {
+            alert("CEP inválido!");
+            return false;
+        }
+    }
+
+    function initialize() {
+        var mapOptions = {
+            center: new google.maps.LatLng(40.680898, -8.684059),
+            zoom: 11,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    }
+
+    google.maps.event.addDomListener(window, "load", initialize);
+
+    function pesquisarEndereco() {
+        var enderecoInput = document.getElementById('texCep').value;
+
+        var geocoder = new google.maps.Geocoder();
+
+        geocoder.geocode({
+            address: map-canvas
+        }, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                var myResult = results[0].geometry.location;
+
+                criarMarcador(myResult);
+
+                map.setCenter(myResult);
+
+                map.setZoom(17);
+            } else {
+                alert("O Geocode não foi bem sucedido pela seguinte razão" + status);
+            }
+        });
+    }
+
+    function criarMarcador(latlng) {
+        if (marker != undefined && marker != '') {
+            marker.setMap(null);
+            marker = '';
+        }
+
+        marker = new google.maps.Marker({
+            map: map,
+            position: latlng
+        });
+    }
+
+
+
+});
